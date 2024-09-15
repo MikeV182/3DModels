8a5d444c7473138869d1cc087d9256f3ba927daf
Mike Vays <michailvays@gmail.com>
2024-09-15 20:23:23 +0700

    Rendered SignUpPage view

--- Main/src/main/java/com/project/Main/controllers/ClientController.java
+++ Main/src/main/java/com/project/Main/controllers/ClientController.java
@@ -5,9 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ClientController {
-
-    @GetMapping()
+    @GetMapping("/")
     public String homePage() {
         return "homePage";
+    }
+
+    @GetMapping("/new")
+    public String signUpPage() {
+        return "SignUpPage";
     }
 }
---
+++ Main/src/main/resources/static/SignUpPageApp.js
@@ -0,0 +1,45 @@
+let passwordEyeIcon = document.getElementById("passwordEyeIcon");
+let passwordField = document.getElementById("PasswordField");
+let hidePassword = true;
+
+let imageIndex = 0;
+let changingPointIndex = 0;
+let imageSrcs = ["img/SignUpPicture1.jpg", "img/SignUpPicture2.jpg", "img/SignUpPicture3.jpg"];
+let pointIds = ["point0", "point1", "point2"];
+let thumbImage = document.getElementById("thumbImage");
+let currentPoint = document.getElementById(pointIds[changingPointIndex]);
+
+function showHidePassword() {
+    if (hidePassword) {
+        passwordEyeIcon.src = "img/passwordShowIcon.svg";
+        passwordField.type = "text";
+        hidePassword = !hidePassword;
+    }
+    else {
+        passwordEyeIcon.src = "img/passwordHideIcon.svg";
+        passwordField.type = "password";
+        hidePassword = !hidePassword;
+    }
+}
+
+setInterval (function() {
+    if (imageIndex == imageSrcs.length) {
+        imageIndex = 0;
+    }
+    setTimeout(() => thumbImage.style.filter = "brightness(40%)", 600);
+    setTimeout(() => thumbImage.src = imageSrcs[imageIndex++], 500);
+    
+    currentPoint.style.width = "40px";
+    currentPoint.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
+
+    changingPointIndex++;
+    if (changingPointIndex == pointIds.length) {
+        changingPointIndex = 0;
+    }
+    currentPoint = document.getElementById(pointIds[changingPointIndex]);
+
+    currentPoint.style.width = "80px";
+    currentPoint.style.backgroundColor = "rgba(255, 255, 255, 1)";
+
+    thumbImage.style.filter = "brightness(0%)";
+}, 4000);
